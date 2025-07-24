4e5d011f50afa87bbb75909f8a9d763fc3e2fb28
nuel 232 <nwankwoala3@gmail.com>
2025-07-24 23:36:18 +0100

    Add intro page and image assets for the application

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatefulWidget {
+  const IntroPage({super.key});
+
+  @override
+  State<IntroPage> createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //logo
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Image.asset('lib/images/nike.png', height: 240),
+              ),
+
+              const SizedBox(height: 48),
+
+              //title
+              const Text(
+                'Just Do It',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  // color: Colors.grey[800],
+                ),
+              ),
+
+              const SizedBox(height: 24),
+
+              //subtitle
+              const Text(
+                'Brand new sneakers and custom kicks made with premium quality',
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+                textAlign: TextAlign.center,
+              ),
+
+              const SizedBox(height: 48),
+
+              //start now button
+              GestureDetector(
+                onTap: () => Navigator.pushNamed(context, '/homePage'),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[900],
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  padding: EdgeInsets.all(25),
+                  child: const Center(
+                    child: const Text(
+                      'shop now',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
